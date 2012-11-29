5880226812901c67106696e0ee1ce030b418c95b
ggrandes <guillermo.grandes@gmail.com>
2012-11-29 05:37:49 +0100

    Generator for RSA keys and X.509 self-signed certificates (SSL/TLS)

--- src/net/bouncer/KeyGenerator.java
+++ src/net/bouncer/KeyGenerator.java
@@ -1,3 +1,17 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package net.bouncer;
 
 import java.security.KeyPair;
@@ -33,10 +47,12 @@ import sun.security.x509.X500Name;
 import sun.security.x509.X509CertImpl;
 import sun.security.x509.X509CertInfo;
 
+/**
+ * Generate RSA keys and X.509 self-signed certificates for SSL/TLS
+ *
+ * @author Guillermo Grandes / guillermo.grandes[at]gmail.com
+ */
 public class KeyGenerator {
-	/**
-	 * Generate RSA keys and X.509 self-signed certificates for SSL/TLS
-	 */
 	public static void main(String[] args) throws Exception {
 		String iam = KeyGenerator.class.getName();
 		if (args.length < 4) {
